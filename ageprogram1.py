e64d58faa0cea6c62515c4d8d160f5ce4ce369aa
asd <jofmc858@gmail.com>
2018-02-24 10:28:43 +1000

    newfiles

---
+++ ageprogram1.py
@@ -0,0 +1,10 @@
+def age_program():
+	seconds_or_years = input("Give me seconds (s) or years (y)? ")
+	if seconds_or_years == "s":
+		#convert seconds to years
+		user_value = input("Enter the number of seconds you've lived for:  ")
+		print("You've lived for {} years".format(int(user_value)/60/60/24/365))
+	elif seconds_or_years == "y":
+		#convrt seconds to years
+		user_value = input("Enter the number of years you've lived for: ")
+		print("You've lived for {} seconds".format(int(user_value) * 365 * 24 * 60 * 60 ))
--- yourageinseconds.py
+++ yourageinseconds.py
@@ -1,4 +1,4 @@
 def age_in_seconds():
     user_age = input('Enter your age: ')
     age_seconds = int(user_age) * 365 * 24 * 60 * 60
-    print('You have lived for () seconds.'.format(age_seconds))
+    print('You have lived for {} seconds.'.format(age_seconds))
